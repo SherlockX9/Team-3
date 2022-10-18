b5462135415856340dd81a83158233ce83022da8
SherlockX9 <contact.m3self@gmail.com>
2022-10-18 11:26:07 +0100

    Added Max function

--- gitTeamProject/src/gitTeamProject/math.java
+++ gitTeamProject/src/gitTeamProject/math.java
@@ -1,5 +1,13 @@
 package gitTeamProject;
 
 public class math {
-
+	
+	public int intMax (int number1, int number2) {
+		if (number1 > number2) {
+			return number1;
+		}
+		return number2;
+	}
+	
+		
 }
